341af6afdb446748db6048f3175d22c58a245f56
JohannBlood <georg-putilov@mail.ru>
2023-11-22 14:18:28 +0300

    Task 6 fix

--- exception/main.cpp
+++ exception/main.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
 #include <stdexcept>
 #include <gtest/gtest.h>
+#include <sstream>
 
 class TMyException : public std::runtime_error {
 public:
-    TMyException(const std::string& message) : std::runtime_error(message) {}
+    TMyException() : std::runtime_error("Custom Exception") {}
 
-    friend std::ostream& operator<<(std::ostream& os, const TMyException& ex) {
-        return os << "Custom Exception: " << ex.what();
+    // Оператор << для записи сопровождающего текста
+    template <typename T>
+    TMyException& operator<<(const T& value) {
+        std::ostringstream stream;
+        stream << value;
+        additionalInfo += stream.str();
+        return *this;
     }
+
+    const char* what() const noexcept override {
+        return additionalInfo.c_str();
+    }
+
+private:
+    std::string additionalInfo;
 };
 
-class DerivedException1 : public TMyException {
-public:
-    DerivedException1(const std::string& message) : TMyException(message) {}
-};
-
-class DerivedException2 : public TMyException {
-public:
-    DerivedException2(const std::string& message) : TMyException(message) {}
-};
+// Производные классы
+class DerivedException1 : public TMyException {};
+class DerivedException2 : public TMyException {};
 
 TEST(CustomExceptionTest, Exception1) {
-    EXPECT_THROW(throw DerivedException1("Exception 1 occurred"), TMyException);
+    EXPECT_THROW(throw DerivedException1() << "Exception 1 occurred", TMyException);
 }
 
 TEST(CustomExceptionTest, Exception2) {
-    EXPECT_THROW(throw DerivedException2("Exception 2 occurred"), TMyException);
+    EXPECT_THROW(throw DerivedException2() << "Exception 2 occurred", TMyException);
 }
 
 int main(int argc, char** argv) {
+    try {
+        throw TMyException() << "Invalid argument x, got " << 42 << " but expected " << 100;
+    } catch (const TMyException& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    try {
+        throw DerivedException1() << "Something went wrong in DerivedException1";
+    } catch (const TMyException& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    try {
+        throw DerivedException2() << "Something went wrong in DerivedException2";
+    } catch (const TMyException& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+    return RUN_ALL_TESTS();}
